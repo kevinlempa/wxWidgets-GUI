61042e90fc6b758a00864457e7e1b5f429953d66
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-06-07 22:38:19 +0200

    adds textfield, list and button.

---
+++ wxWidgets/wxWidgets/cMain.cpp
@@ -0,0 +1,20 @@
+#include "cMain.h"
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+EVT_BUTTON(10001, OnButtonClicked)
+wxEND_EVENT_TABLE()
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Testing wxWidgets!", wxPoint(1600, 30), wxSize(1280, 720)) {
+	m_btn1 = new wxButton(this, 10001, "Click Me", wxPoint(10, 10), wxSize(150, 50));
+	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
+	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+
+}
+cMain::~cMain() {
+
+}
+
+void cMain::OnButtonClicked(wxCommandEvent& evt)
+{
+	m_list1->AppendString(m_txt1->GetValue());
+	evt.Skip();
+}
